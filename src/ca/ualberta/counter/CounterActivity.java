c2af9d48335fdfc0318f93fbe3d7e7b292d784fa
alexgfh <alexgfh@gmail.com>
2014-01-22 14:18:58 -0700

    Started the GUI

--- src/ca/ualberta/counter/CounterActivity.java
+++ src/ca/ualberta/counter/CounterActivity.java
@@ -1,14 +1,13 @@
 package ca.ualberta.counter;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {
-
+public class CounterActivity extends Activity {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
 }

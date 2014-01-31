f29c283470d59b68be21aac603d2134bee2c3349
alexgfh <alexgfh@gmail.com>
2014-01-31 00:18:45 -0700

    Refactored Calendar logic out of Counter Model into a new class

---
+++ src/ca/ualberta/counter/CalendarList.java
@@ -0,0 +1,113 @@
+package ca.ualberta.counter;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+
+/*
+ * Class that wraps an ArrayList<Calendar> and extends it with
+ * functions that provides statistics by time periods.
+ */
+
+public class CalendarList {
+	private ArrayList<Calendar> calendarList;
+
+	public CalendarList() {
+		calendarList = new ArrayList<Calendar>();
+	}
+	
+	public void addNow() {
+		calendarList.add(Calendar.getInstance());
+	}
+	
+	/*
+	 * Each of the following four methods returns an ArrayList with the
+	 * statistics to be printed.
+	 * First a HashMap is filled with keys, which are Strings that represents
+	 * the Date for that format, with values that are the counts of that Date.
+	 * Then, an ArrayList is filled with each String followed by it's count.
+	 */
+	
+	public ArrayList<String> getStatisticsByHour() {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for(Calendar cal : calendarList) {
+			String key = cal.get(Calendar.YEAR) + " " + 
+					cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault()) + " " +
+					cal.get(Calendar.DAY_OF_MONTH) + " " +
+					cal.get(Calendar.HOUR_OF_DAY) + ":00";
+			if(map.containsKey(key)) {
+				map.put(key, map.get(key)+1);
+			}
+			else {
+				map.put(key, 1);
+			}
+		}
+		ArrayList<String> stats = new ArrayList<String>();
+		for(String key : map.keySet()) {
+			stats.add(key + " -- " + map.get(key));
+		}
+		return stats;
+	}
+
+	public ArrayList<String> getStatisticsByDay() {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for(Calendar cal : calendarList) {
+			String key = cal.get(Calendar.YEAR) + " " + 
+					cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault()) + " " +
+					cal.get(Calendar.DAY_OF_MONTH);
+			if(map.containsKey(key)) {
+				map.put(key, map.get(key)+1);
+			}
+			else {
+				map.put(key, 1);
+			}
+		}
+		ArrayList<String> stats = new ArrayList<String>();
+		for(String key : map.keySet()) {
+			stats.add(key + " -- " + map.get(key));
+		}
+		return stats;
+	}
+
+	public ArrayList<String> getStatisticsByWeek() {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for(Calendar cal : calendarList) {
+			String key = cal.get(Calendar.YEAR) + " " + 
+					cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault())
+					+ " Week " +
+					cal.get(Calendar.WEEK_OF_MONTH);
+			if(map.containsKey(key)) {
+				map.put(key, map.get(key)+1);
+			}
+			else {
+				map.put(key, 1);
+			}
+		}
+		ArrayList<String> stats = new ArrayList<String>();
+		for(String key : map.keySet()) {
+			stats.add(key + " -- " + map.get(key));
+		}
+		return stats;
+	}
+
+	public ArrayList<String> getStatisticsByMonth() {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for(Calendar cal : calendarList) {
+			String key = cal.get(Calendar.YEAR) + " " + 
+					cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault());
+			if(map.containsKey(key)) {
+				map.put(key, map.get(key)+1);
+			}
+			else {
+				map.put(key, 1);
+			}
+		}
+		ArrayList<String> stats = new ArrayList<String>();
+		for(String key : map.keySet()) {
+			stats.add(key + " -- " + map.get(key));
+		}
+		return stats;
+	}
+
+}
--- src/ca/ualberta/counter/CounterModel.java
+++ src/ca/ualberta/counter/CounterModel.java
@@ -2,9 +2,6 @@ package ca.ualberta.counter;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Locale;
 
 /**
  * Models an individual counter.
@@ -13,18 +10,17 @@ import java.util.Locale;
 public class CounterModel implements Comparable<CounterModel>, Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private ArrayList<Calendar> counts;
+	private CalendarList timeStamps = new CalendarList();
 	private int counterValue;
 	private String name;
 
 	public CounterModel(String name) {
 		counterValue=0;
 		this.name = name;
-		counts = new ArrayList<Calendar>();
 	}
 
 	public void count() {
-		counts.add(Calendar.getInstance());
+		timeStamps.addNow();
 		counterValue++;
 	}
 
@@ -42,96 +38,7 @@ public class CounterModel implements Comparable<CounterModel>, Serializable {
 	
 	public String getName() {
 		return name;
-	}
-	
-	/*
-	 * Each of the following four methods returns an ArrayList with the
-	 * statistics to be printed.
-	 * First a HashMap is filled with keys, which are Strings that represents
-	 * the Date for that format, with values that are the counts of that Date.
-	 * Then, an ArrayList is filled with each String followed by it's count.
-	 */
-	
-	public ArrayList<String> getStatisticsByHour() {
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		for(Calendar cal : counts) {
-			String key = cal.get(Calendar.YEAR) + " " + 
-					cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault()) + " " +
-					cal.get(Calendar.DAY_OF_MONTH) + " " +
-					cal.get(Calendar.HOUR_OF_DAY) + ":00";
-			if(map.containsKey(key)) {
-				map.put(key, map.get(key)+1);
-			}
-			else {
-				map.put(key, 1);
-			}
-		}
-		ArrayList<String> stats = new ArrayList<String>();
-		for(String key : map.keySet()) {
-			stats.add(key + " -- " + map.get(key));
-		}
-		return stats;
-	}
-
-	public ArrayList<String> getStatisticsByDay() {
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		for(Calendar cal : counts) {
-			String key = cal.get(Calendar.YEAR) + " " + 
-					cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault()) + " " +
-					cal.get(Calendar.DAY_OF_MONTH);
-			if(map.containsKey(key)) {
-				map.put(key, map.get(key)+1);
-			}
-			else {
-				map.put(key, 1);
-			}
-		}
-		ArrayList<String> stats = new ArrayList<String>();
-		for(String key : map.keySet()) {
-			stats.add(key + " -- " + map.get(key));
-		}
-		return stats;
-	}
-
-	public ArrayList<String> getStatisticsByWeek() {
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		for(Calendar cal : counts) {
-			String key = cal.get(Calendar.YEAR) + " " + 
-					cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault())
-					+ " Week " +
-					cal.get(Calendar.WEEK_OF_MONTH);
-			if(map.containsKey(key)) {
-				map.put(key, map.get(key)+1);
-			}
-			else {
-				map.put(key, 1);
-			}
-		}
-		ArrayList<String> stats = new ArrayList<String>();
-		for(String key : map.keySet()) {
-			stats.add(key + " -- " + map.get(key));
-		}
-		return stats;
-	}
-
-	public ArrayList<String> getStatisticsByMonth() {
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		for(Calendar cal : counts) {
-			String key = cal.get(Calendar.YEAR) + " " + 
-					cal.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault());
-			if(map.containsKey(key)) {
-				map.put(key, map.get(key)+1);
-			}
-			else {
-				map.put(key, 1);
-			}
-		}
-		ArrayList<String> stats = new ArrayList<String>();
-		for(String key : map.keySet()) {
-			stats.add(key + " -- " + map.get(key));
-		}
-		return stats;
-	}
+	}	
 
 	//This is the String printed in the CounterBrowserActivity List.
 	@Override
@@ -148,6 +55,22 @@ public class CounterModel implements Comparable<CounterModel>, Serializable {
 			return 0;
 		else
 			return 1;
+	}
+
+	public ArrayList<String> getStatisticsByHour() {
+		return timeStamps.getStatisticsByHour();
+	}
+	
+	public ArrayList<String> getStatisticsByDay() {
+		return timeStamps.getStatisticsByDay();
+	}
+	
+	public ArrayList<String> getStatisticsByWeek() {
+		return timeStamps.getStatisticsByWeek();
+	}
+	
+	public ArrayList<String> getStatisticsByMonth() {
+		return timeStamps.getStatisticsByMonth();
 	}
 
 }
